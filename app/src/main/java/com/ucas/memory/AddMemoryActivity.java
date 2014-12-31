5bddf0c90f0f882598417f046bcabb894f948f1b
luzhitong <lzt15117933093@163.com>
2014-12-31 19:19:40 +0800

    update
    before camera function

--- app/src/main/java/com/ucas/memory/AddMemoryActivity.java
+++ app/src/main/java/com/ucas/memory/AddMemoryActivity.java
@@ -1,18 +1,52 @@
 package com.ucas.memory;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Created by w on 2014/12/30.
  */
-public class AddMemoryActivity extends Activity{
+public class AddMemoryActivity extends Activity {
+
+    ImageView addPhoto;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_write_message);
 
-    }
+        addPhoto = (ImageView) findViewById(R.id.IB_add);
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
 
     }
+
+
+}
